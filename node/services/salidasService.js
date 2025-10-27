4a526e1e03867f9448f0c86de3c24b404dc90e46
ELPERRON <linkonv10@gmail.com>
2025-10-27 12:07:43 -0500

    Copie y pegue los archivos de mi tabla salidas

---
+++ node/services/salidasService.js
@@ -0,0 +1,33 @@
+import { where } from "sequelize";
+import Salidas from "../Models/Salidas.js";
+
+class Ser_Salida{
+
+    async getAllSalidas(){
+        return await Salidas.findAll()
+    }
+
+    async getSalidaById(Id_Salida){
+
+    const salida = await Salidas.findByPk(Id_Salida)
+    if (!salida) throw new Error("Envio no encontrado")
+    return salida
+    }
+    async create(data){
+        return await Salidas.create(data)
+    }
+
+    async update(Id_Salida, data){
+        const result = await Salidas.update(data,{ where: {Id_Salida}})
+        const update = result[0]
+        if (update === 0 ) throw new Error ("Envio no encontrado o Datos no actualizados")
+        return true 
+    }
+    async delete(Id_Salida){
+        const deleted = await Salidas.destroy({where: {Id_Salida}})
+        if(!deleted) throw new Error ("Envio no encontrado o no eliminado")
+        return true
+    }
+    
+}
+export default new Ser_Salida()
