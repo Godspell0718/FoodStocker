6f1a5a9c8b80ba269ee6e6eafb20f35767fadf36
Andrey <melotrejosandreyfelipe@gmail.com>
2025-10-28 07:55:30 -0500

    Se agregaron los import y las api que faltaban

--- node/app.js
+++ node/app.js
@@ -1,10 +1,15 @@
 import express from "express"
 import cors from "cors"
 import db from "./database/db.js"
-import pasantesrouters from "./routes/pasanteRouters.js"
-import proveedoresRouters from "./routes/proveedoresRouters.js"
+import almacenRouters from "/routes/almacenRouters.js" 
+import destinoRoutes from "/routes/destinoRoutes.js"
 import entradasRoutes from "./routes/entradasRoutes.js"
 import insumosProveedorRoutes from "./routes/insumosProveedorRoutes.js"
+import insumosRouters from "/routes/insumosRouters.js" 
+import loteRoutes from "/routes/loteRoutes.js"
+import pasantesRouters from "./routes/pasanteRouters.js"
+import proveedoresRouters from "./routes/proveedoresRouters.js"
+import salidasRoute from "/routes/salidasRoute.js"
 
 import dotenv from "dotenv"
 
@@ -15,13 +20,15 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.use("/api/pasantes", pasantesrouters)
-app.use("/api/proveedores", proveedoresRouters)
+app.use("/api/almacen", almacenRouters)
+app.use("/api/destino", destinoRoutes)
 app.use("/api/entradas", entradasRoutes)
 app.use("/api/insumosproveedor", insumosProveedorRoutes)
-app.use("/api/almacenRouters", almacenRouters.js)
-app.use("/api/insumosRouters", insumosRouters.js)
-
+app.use("/api/insumos", insumosRouters)
+app.use("/api/lote", loteRoutes)
+app.use("/api/pasantes", pasantesRouters)
+app.use("/api/proveedores", proveedoresRouters)
+app.use("/api/salidas", salidasRoute)
 
 
 try{
